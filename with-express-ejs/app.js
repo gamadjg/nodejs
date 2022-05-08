545ff06408e92868208090a3dd5ca22447a73000
gamadjg <gamadavid36@gmail.com>
2022-05-08 10:16:50 -0700

    Installed Express and EJS dependencies. refactored routes with expresss, implimented partials and static files

---
+++ with-express-ejs/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+app.use(express.static("public"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.listen(port);
+
+app.get("/", (req, res) => {
+	const todoList = [
+		// { priority: 1, due: "today", task: "Pet dog" },
+		// { priority: 2, due: "today", task: "Submit taxes" },
+		// { priority: 2, due: "today", task: "Complete ejs tutorial" },
+	];
+	//res.sendFile("index", { root: __dirname });
+	res.render("index", { title: "Home", todoList });
+	console.log("req made to enter home page.");
+});
+
+app.get("/about", (req, res) => {
+	//res.sendFile("./views/about.html", { root: __dirname });
+	res.render("about", { title: "About" });
+});
+
+app.get("/about-me", (req, res) => {
+	//res.redirect("/about.html");
+	res.redirect("about");
+});
+
+app.get("/contact-me", (req, res) => {
+	//res.sendFile("./views/contact-me.html", { root: __dirname });
+	res.render("contact-me", { title: "Contact" });
+});
+
+app.use((req, res) => {
+	//res.sendFile("./views/404.html", { root: __dirname });
+	res.render("404");
+});
