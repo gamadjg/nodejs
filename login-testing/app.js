b1e0f08a6dcfd17b9e01125fb1c39c6b11f80872
gamadjg <gamadavid36@gmail.com>
2022-06-19 07:08:25 -0700

    Added db connector, setup sign in form

--- login-testing/app.js
+++ login-testing/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
@@ -6,7 +7,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const mongoDb = "YOUR MONGO URL HERE";
+const mongoDb = process.env.dbURI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -20,7 +21,7 @@ const User = mongoose.model(
 );
 
 const app = express();
-app.set("views", __dirname);
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
@@ -28,6 +29,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { title: "Home" }));
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", (req, res, next) => {
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password,
+	}).save((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
