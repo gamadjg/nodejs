4d1d664bc45deabdda0d0c2971f74b686d721d80
gamadjg <gamadavid36@gmail.com>
2022-06-22 17:37:31 -0700

    bcrypt setup

--- login-testing/app.js
+++ login-testing/app.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const mongoDb = process.env.dbURI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -22,6 +23,7 @@ const User = mongoose.model(
 
 const app = express();
 app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
@@ -29,15 +31,70 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index", { title: "Home" }));
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		User.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					return done(null, user);
+				} else {
+					// passwords do not match!
+					return done(null, false, { message: "Incorrect password" });
+				}
+			});
+			return done(null, user);
+		});
+	})
+);
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+	res.render("index", { title: "Home", user: req.user });
+});
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", (req, res, next) => {
-	const user = new User({
-		username: req.body.username,
-		password: req.body.password,
-	}).save((err) => {
+	bcrypt.hash(req.body.password, 10, (err, hashedPwd) => {
+		const user = new User({
+			username: req.body.username,
+			password: hashedPwd,
+		}).save((err) => {
+			if (err) {
+				return next(err);
+			}
+			res.redirect("/");
+		});
+	});
+});
+
+app.post(
+	"/log-in",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/",
+	})
+);
+
+app.get("/log-out", (req, res) => {
+	req.logout(function (err) {
 		if (err) {
 			return next(err);
 		}
